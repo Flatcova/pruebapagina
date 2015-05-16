16d2b93e69b3f8912bd6692dbe0cb607846712f1
Flatcova <m_covar@hotmail.com>
2015-05-15 19:36:33 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+  $('li').hover(
+    function(){
+        $(this).addClass('active');
+    },
+    function(){
+        $(this).removeClass('active');
+    }
+  );
+
+});
